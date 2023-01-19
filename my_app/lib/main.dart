c1f3403886f57024129b817cb10a43ea7fd92233
seerbann <serbypwp@yahoo.com>
2023-01-19 21:12:54 +0200

    Update main.dart

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -73,7 +73,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   children: [
                     Container(
                       width: 300,
-                      padding: const EdgeInsets.all(15),
                       child: const Text(
                         "Coloana1orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                         maxLines: 35,
@@ -83,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       width: 150,
                     ),
                     Container(
-                      width: 400,
+                      width: 300,
                       child: Text(
                         "Coloana2 t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
                         maxLines: 35,
