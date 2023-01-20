e7de56a1a23bc256addd9757fd1677ebb53822d1
seerbann <serbypwp@yahoo.com>
2023-01-20 13:29:03 +0200

    finally 2 columns

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -39,6 +39,10 @@ _launchURLBrowser() async {
   }
 }
 
+var description = Container(
+  child: Text('descrierea mea'),
+);
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   void _incrementCounter() {
@@ -50,68 +54,37 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-              icon: Image.asset(
-                './assets/github-mark-white.png',
-                fit: BoxFit.cover,
-              ),
-              onPressed: () {
-                _launchURLBrowser();
-              }),
-          centerTitle: true,
-          title: Text("Chiriac Serban-Mihail"),
-        ),
-        body: Container(
-            padding: const EdgeInsets.all(32),
-            child: Column(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Image.asset(
+              './assets/github-mark-white.png',
+              fit: BoxFit.cover,
+            ),
+            onPressed: () {
+              _launchURLBrowser();
+            }),
+        centerTitle: true,
+        title: Text("Chiriac Serban-Mihail"),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(
-                    child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 300,
-                      child: const Text(
-                        "Coloana1orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                        maxLines: 35,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 150,
-                    ),
-                    Container(
-                      width: 300,
-                      child: Text(
-                        "Coloana2 t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-                        maxLines: 35,
-                      ),
-                    ),
-                  ],
-                )),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 300,
-                      child: Text(
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                      ),
-                    ),
-                    SizedBox(
-                      width: 150,
-                    ),
-                    Container(
-                      width: 300,
-                      child: Text(
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                      ),
-                    )
-                  ],
+                Column(
+                  children: [description, Text('DATA')],
                 ),
-              ],
-            )));
+                SizedBox(
+                  width: 550,
+                ),
+                Column(
+                  children: [Text('otger column'), Text('otger column')],
+                )
+              ]),
+        ),
+      ),
+    );
   }
 }
