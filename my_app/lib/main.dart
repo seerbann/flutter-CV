7e342507a41a712ef30731ac92010fb0ba87fe3e
seerbann <serbypwp@yahoo.com>
2023-01-24 18:17:53 +0200

    Update main.dart

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -13,10 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'CV-Chiriac Serban',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-        secondaryHeaderColor: Colors.grey,
-      ),
+      theme: ThemeData(primarySwatch: Colors.grey),
       home: const MyHomePage(title: 'Home'),
     );
   }
@@ -41,16 +38,33 @@ _launchURLBrowser() async {
 
 var description = Container(
   child: Text(
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  ),
+      "       Highly motivated computer science student with a strong interest in sports and a strong and dedicated team player. Working well in a team environment, as well as excellent communication and interpersonal skills. Seeking to leverage technical skills to cotribute to a dynamic organization. ",
+      style: TextStyle(fontSize: 14.0, height: 1.5)),
+);
+
+var dots = Column(
+  children: [
+    Text("\u2022 myMail@gmail.com"),
+  ],
+);
+
+var education = Column(
+  children: [
+    SizedBox(
+      height: 15,
+    ),
+    Text("October, 2022 - Present"),
+    SizedBox(
+      height: 10,
+    ),
+    Text("\u2022 Faculty of Computer Science - Iasi")
+  ],
 );
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    setState(() {});
   }
 
   @override
@@ -58,6 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
+            hoverColor: Colors.white.withOpacity(0.3),
             icon: Image.asset(
               './assets/github-mark-white.png',
               fit: BoxFit.cover,
@@ -65,8 +80,10 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: () {
               _launchURLBrowser();
             }),
-        centerTitle: true,
-        title: Text("Chiriac Serban-Mihail"),
+        title: Text(
+          "Chiriac Serban-Mihail",
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
       ),
       body: Container(
         child: Padding(
@@ -76,6 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Expanded(
+                  flex: 4,
                   child: Column(
                     children: [
                       const Text(
@@ -89,7 +107,23 @@ class _MyHomePageState extends State<MyHomePage> {
                       SizedBox(
                         height: 20,
                       ),
-                      description
+                      description,
+                      SizedBox(
+                        height: 20,
+                      ),
+                      dots,
+                      SizedBox(
+                        height: 100,
+                      ),
+                      Text(
+                        "Education",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      education,
                     ],
                   ),
                 ),
@@ -97,33 +131,35 @@ class _MyHomePageState extends State<MyHomePage> {
                   width: 350,
                 ),
                 Expanded(
+                    flex: 6,
                     child: Column(
-                  children: [
-                    Container(
-                      //color: Color.fromRGBO(226, 220, 220, 1),
-                      child: const Text(
-                        'Projects',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 40,
-                          color: Colors.grey,
+                      children: [
+                        Container(
+                          //color: Color.fromRGBO(226, 220, 220, 1),
+                          child: const Text(
+                            'Projects',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 40,
+                              color: Colors.grey,
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text('\u2022 Bullet Text'),
-                    SizedBox(height: 10),
-                    Text('\u2022 Bullet Text'),
-                    SizedBox(height: 10),
-                    Text('\u2022 Bullet Text'),
-                    SizedBox(height: 10),
-                    Text('\u2022 Bullet Text'),
-                    SizedBox(height: 10),
-                    Text('\u2022 Bullet Text'),
-                  ],
-                )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                            "\u2022 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived"),
+                        SizedBox(height: 10),
+                        Text('\u2022 Bullet Text'),
+                        SizedBox(height: 10),
+                        Text('\u2022 Bullet Text'),
+                        SizedBox(height: 10),
+                        Text('\u2022 Bullet Text'),
+                        SizedBox(height: 10),
+                        Text('\u2022 Bullet Text'),
+                      ],
+                    )),
               ]),
         ),
       ),
